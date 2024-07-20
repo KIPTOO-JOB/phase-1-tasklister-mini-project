1a3c9d50256ca2cd10371877f302e6aa227c67b5
JOB KIPTOO <114221274+KIPTOO-JOB@users.noreply.github.com>
2024-07-21 00:45:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const p = document.querySelector('p');
+  if (p) {
+    p.textContent = 'This is really cool!';
+  }
 });
